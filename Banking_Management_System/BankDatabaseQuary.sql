04e189f76822d0ef5035487778f37b9858786ba9
pritamdas022 <110466018+pritamdas022@users.noreply.github.com>
2022-12-09 01:46:37 +0530

    Update BankDatabaseQuary.sql

--- Banking_Management_System/BankDatabaseQuary.sql
+++ Banking_Management_System/BankDatabaseQuary.sql
@@ -8,3 +8,13 @@ branch varchar(10),
 acc_type varchar(10),
 balance numeric(12,2) check(balance>1999),
 pin numeric(4) unique not null);
+
+
+use pritamdb;
+create table Bank(account_no integer,
+account_holder varchar(20) primary key,
+ifsc_code varchar(20),
+branch varchar(10),
+account_type varchar(10),
+balance numeric(12,2),
+pin numeric(4));
